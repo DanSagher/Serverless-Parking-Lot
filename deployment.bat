fff530f4da9f4517b3a9eb617e5fc94d1d1fdbf9
DanSagher <73784282+DanSagher@users.noreply.github.com>
2021-05-11 22:17:25 +0300

    Updatet deployment bat
    create bucket before deployment.

--- deployment.bat
+++ deployment.bat
@@ -1,4 +1,6 @@
 
+aws s3api create-bucket --bucket 31dfsaoopo91 --region eu-central-1 --create-bucket-configuration LocationConstraint=eu-central-1
+
 dotnet build
 
-dotnet lambda deploy-serverless ParkingLotDanAndTal
+dotnet lambda deploy-serverless ParkingLotDanAndTal --s3-bucket 31dfsaoopo91
