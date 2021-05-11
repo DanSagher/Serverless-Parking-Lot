ce8015f4ba8c4f3e9b3e8b1fb7c4fa5a2d4b256a
DanSagher <73784282+DanSagher@users.noreply.github.com>
2021-05-11 22:56:07 +0300

    Update deployment.bat

--- deployment.bat
+++ deployment.bat
@@ -1,6 +1,6 @@
 
-aws s3api create-bucket --bucket 31dfsaoopo91 --region eu-central-1 --create-bucket-configuration LocationConstraint=eu-central-1
+aws s3api create-bucket --bucket 31dfsaoopo912 --region eu-central-1 --create-bucket-configuration LocationConstraint=eu-central-1
 
 dotnet build
 
-dotnet lambda deploy-serverless ParkingLotDanAndTal --s3-bucket 31dfsaoopo91
+dotnet lambda deploy-serverless ParkingLotDanAndTal --s3-bucket 31dfsaoopo912
