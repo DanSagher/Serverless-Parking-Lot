5def7745b3b8485d22f67809ac10dbccacd16777
DanSagher <73784282+DanSagher@users.noreply.github.com>
2021-05-12 17:26:44 +0300

    update get region first

--- deployment.bat
+++ deployment.bat
@@ -1,6 +1,9 @@
+FOR /F "tokens=* USEBACKQ" %%F IN (`aws configure get region`) DO (
+SET regionName=%%F
+)
 
-aws s3api create-bucket --bucket 31dfsaoopo912 --region eu-central-1 --create-bucket-configuration LocationConstraint=eu-central-1
+aws s3api create-bucket --bucket 31dfsaoopo912 --region %regionName% --create-bucket-configuration LocationConstraint=%regionName%
 
 dotnet build
 
-dotnet lambda deploy-serverless ParkingLotDanAndTal --s3-bucket 31dfsaoopo912
+dotnet lambda deploy-serverless ParkingLotDanAndTal --region %regionName% --s3-bucket 31dfsaoopo912
